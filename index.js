562b97eb0bf5443e5179a66abb4dc69085d8638c
blessed-scope-5001 <roxanne.harris@yale.edu>
2019-08-03 03:20:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -7,9 +7,9 @@ class Formatter {
   static sanitize(str) {
     let result = str.split(" ").map(w => {
       let a = w.replace(/\W/, '');
-      let b = a//.replace(/'/, '');
+      let b = a.replace(/'/, '');
 
-      return a.replace(/@#/, '');
+      return b.replace(/@#/, '');
       // return w.replace(/\W/, '')
     }).join(" ")
     // let result = str.replace(/\W/, '')
